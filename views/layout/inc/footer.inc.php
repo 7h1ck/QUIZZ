285daa85c7a98cd0c066d96cd472f612dddd1f65
Cheikh Ibra YADE <61935826+7h1ck@users.noreply.github.com>
2020-05-31 20:20:30 +0000

    save user and player

--- views/layout/inc/footer.inc.php
+++ views/layout/inc/footer.inc.php
@@ -1,6 +1,19 @@
-
-
+</div>
 
 </body>
+<script>
+    function prevUpload()
+{
+    document.getElementById('error_f').innerText="";
+    var file = document.getElementById("imgUser").files[0];
+    var reader = new FileReader();
+    if (file) {
+        reader.readAsDataURL(file);
+        reader.onloadend = function () {
+        document.getElementById("avatar").src = reader.result;
+        }
+    }
+}
+</script>
 
 </html>
--- views/layout/inc/header.inc.php
+++ views/layout/inc/header.inc.php
@@ -8,15 +8,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=WEBROOT?>/assets/css/classe.css">
+    <link rel="stylesheet" href="<?=URL_ASSETS?>/css/bootstrap.min.css">
 
 
 </head>
 
 <body>
 
-    <header class="bgSecondary">
-        <h1 class="text-center m-0">
-            Le plaisir de jouer
-        </h1>
-    </header>
+    <!-- <header> -->
+    <nav class="navbar navbar-dark d-flex justify-content-start" style="background-color: #042425;">
+                <a class="navbar-brand" href="#">
+                    <img src="<?=URL_ASSETS?>/img/logo-QuizzSA.png" alt="logo" style="width:40px;">
+                </a>
+                <h2 class="text-white text-center flex-grow-1">Le plaisir de jouer</h2>
+            </nav>
+
+
+    <div class="d-flex justify-content-center align-items-center" style="background:url(<?=URL_ASSETS?>/img/background.png) no-repeat; height: 850px;background-size: cover;">
